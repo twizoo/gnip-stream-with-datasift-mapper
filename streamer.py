2ae2205246d5db6f0ac2cc237fc640b6cbd4c65a
gilboise <talbott@talk21,com>
2015-06-15 19:43:31 +0100

    Added a comment RE: doing interesting things with your tweet

--- streamer.py
+++ streamer.py
@@ -13,6 +13,7 @@ def callback(event):
     if len(event) > 0:
         tweet_json = json.loads(event)
 
+        # Do something more interesting with your tweet here
         if config.MAP_TO_DATASIFT_FORMAT:
             datasift_tweet_json = tweet_mapper.map_from_gnip_to_datasift(tweet_json)
             print "------- DATASIFT MAPPED TWEET -------"
